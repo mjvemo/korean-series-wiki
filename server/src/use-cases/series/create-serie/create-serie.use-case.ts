7cd35208a82c82134a47362d8b0bba6fb7119aa1
mjvemo <74282064+mjvemo@users.noreply.github.com>
2023-12-15 19:18:25 -0600

    add serie id to actors

--- server/src/use-cases/series/create-serie/create-serie.use-case.ts
+++ server/src/use-cases/series/create-serie/create-serie.use-case.ts
@@ -1,17 +1,35 @@
-import defaultRepo, {
+import defaultSerieRepo, {
   SerieRepository,
 } from "../../../domain/serie/repositories/serie.repository";
+import defaultActorRepo, {
+  ActorRepository,
+} from "../../../domain/actor/repositories/actor.repository";
 import { Serie } from "../../../domain/serie/serie";
 import { SerieDTO } from "../../../dtos/serie.dto";
 import { CreateSerieRequestDTO } from "./create-serie-request.dto";
 
 export class CreateSerieUseCase {
-  constructor(private repo: SerieRepository = defaultRepo) {}
+  constructor(
+    private serieRepo: SerieRepository = defaultSerieRepo,
+    private actorRepo: ActorRepository = defaultActorRepo
+  ) {}
 
   async create(request: CreateSerieRequestDTO): Promise<SerieDTO> {
     const serie = Serie.create(request);
 
-    await this.repo.save(serie);
+    await this.serieRepo.save(serie);
+
+    for (const actorId of serie.cast) {
+      const actor = await this.actorRepo.findById(actorId);
+
+      if (actor) {
+        actor.addSerie(serie.id);
+
+        console.log(`Adding serie to actor ${serie.id} and ${actorId}`);
+
+        await this.actorRepo.save(actor);
+      }
+    }
 
     return serie.serialize();
   }
